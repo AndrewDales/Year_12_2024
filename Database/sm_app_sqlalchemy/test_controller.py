e32d7c930b9d846abb9e4c7ed04ae365383e5925
dalesa <andrew.dales@highgateschool.org.uk>
2025-03-10 18:44:00 +0000

    Added a test for adding posts

--- Database/sm_app_sqlalchemy/test_controller.py
+++ Database/sm_app_sqlalchemy/test_controller.py
@@ -10,18 +10,17 @@ from Database.sm_app_sqlalchemy.controller import Controller
 # test_db_location = 'sqlite:///:memory:'
 test_db_location = 'sqlite:///test_sm.db'
 
-@pytest.fixture(scope="module")
-def db_session():
-    engine = sa.create_engine(test_db_location, echo=False)
-    Base.metadata.create_all(engine)
-    session = so.Session(bind=engine)
-    yield session
-    session.rollback()
-    session.close()
-    Base.metadata.drop_all(engine)
-
 
 class TestDataBase:
+    @pytest.fixture(scope="class")
+    def db_session(self):
+        engine = sa.create_engine(test_db_location, echo=False)
+        Base.metadata.create_all(engine)
+        session = so.Session(bind=engine)
+        yield session
+        session.close()
+        Base.metadata.drop_all(engine)
+
     def test_valid_user(self, db_session):
         """ Checks that a User can be added and retrieved from the database"""
         user = User(name = "Bob Dylan", age=81, gender="male")
@@ -41,8 +40,23 @@ class TestDataBase:
         db_session.add(user)
         with pytest.raises(IntegrityError):
             db_session.commit()
+        db_session.rollback()
 
-
+    def test_add_post(self, db_session):
+        """ Test creating a user and adding a post to that user
+        - check that the post is including in user.posts and the user
+        is included in post.user"""
+        user = User(name="Eleonore", age=20, gender="female")
+        post = Post(title="Hello", description="Hello World")
+        user.posts.append(post)
+        db_session.add(user)
+        db_session.commit()
+        eleonore = db_session.scalar(sa.select(User).where(User.name == "Eleonore"))
+        assert eleonore.posts == [post,]
+        e_post = eleonore.posts[0]
+        assert e_post.title == "Hello"
+        assert e_post.description == "Hello World"
+        assert e_post.user == user
 
 class TestController:
     @pytest.fixture(scope="class", autouse=True)
